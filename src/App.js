d841cd4cab18302622ea61fd30864bcf98c3fe5a
Alhassan Osman <oalhassan847@gmail.com>
2023-02-14 21:28:02 +0000

    Refactor and improve code

--- src/App.js
+++ src/App.js
@@ -4,28 +4,7 @@ import Calculator from './components/Calculator';
 function App() {
   return (
     <div className="App">
-      <Calculator
-        one="1"
-        two="2"
-        three="3"
-        four="4"
-        five="5"
-        six="6"
-        seven="7"
-        eight="8"
-        nine="9"
-        ac="AC"
-        plusminus="+/-"
-        modulo="%"
-        point="."
-        output={0}
-        plus="+"
-        zero="0"
-        equal="="
-        multi="x"
-        divide="÷"
-        minus="-"
-      />
+      <Calculator output="0" />
     </div>
 
   );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,56 +1,45 @@
+import { ReactPropTypes } from 'react';
+
+const Button = ({
+  className, value, onClick, id,
+}) => (
+  <button type="button" className={className} onClick={onClick} id={id}>
+    {value}
+  </button>
+);
+
+const buttons = [
+  ['AC', '+/-', '%', '÷'],
+  [7, 8, 9, 'x'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '='],
+];
+
+Button.propTypes = {
+  className: ReactPropTypes.string.isRequired,
+  value: ReactPropTypes.string.isRequired,
+  onClick: ReactPropTypes.func.isRequired,
+  id: ReactPropTypes.string.isRequired,
+};
+
 function Calculator(prop) {
-  const {
-    one,
-    two,
-    three,
-    four,
-    five,
-    six,
-    seven,
-    eight,
-    nine,
-    ac,
-    plusminus,
-    modulo,
-    point,
-    output,
-    plus,
-    zero,
-    equal,
-    minus,
-    divide,
-    multi,
-  } = prop;
+  const { output } = prop;
   return (
     <div className="calculator">
       <div className="number-screen">
         {output}
       </div>
       <div className="input">
-        <div className="number-input">
-          <li>{ac}</li>
-          <li>{plusminus}</li>
-          <li>{modulo}</li>
-          <li>{one}</li>
-          <li>{two}</li>
-          <li>{three}</li>
-          <li>{four}</li>
-          <li>{five}</li>
-          <li>{six}</li>
-          <li>{seven}</li>
-          <li>{eight}</li>
-          <li>{nine}</li>
-          <li id="zero">{zero}</li>
-
-          <li>{point}</li>
-        </div>
-        <div className="operation">
-          <li>{divide}</li>
-          <li>{multi}</li>
-          <li>{minus}</li>
-          <li>{plus}</li>
-          <li>{equal}</li>
-        </div>
+        {buttons.flat().map((btn, i) => (
+          <Button
+            key={i}
+            id={`id-${i}`}
+            className={btn === 0 ? 'zero' : 'number-input'}
+            value={btn}
+            onClick
+          />
+        ))}
       </div>
 
     </div>
