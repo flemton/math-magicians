2173882065d5720fac9192dc09e63ba659f159ba
Alhassan Osman <oalhassan847@gmail.com>
2023-02-22 01:56:37 +0000

    Add navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,2 +1,20 @@
-const Navbar = () => <div>Navigation items here</div>;
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'calculator', text: 'Calculator' },
+  { path: 'quotes', text: 'Quotes' },
+];
+
+const Navbar = () => (
+  <nav className="nav">
+    <ul>
+      {links.map((link) => (
+        <li key={link.text}>
+          <NavLink to={link.path}>{link.text}</NavLink>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
 export default Navbar;
